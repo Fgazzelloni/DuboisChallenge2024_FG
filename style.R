cffe5af92ce6a1eda83361d161db66a4bd64612d
Federica Gazzelloni <docksbox@pm.me>
2024-03-04 15:04:17 +0100

    challenge05

---
+++ style.R
@@ -0,0 +1,41 @@
+# DuBois Style ####
+
+# Fonts #### 
+
+# Public Sans
+# Charter
+# DU BOIS (vocaltype)
+
+# library(tidyverse)
+# library(showtext)
+# font_add(family = "Public Sans Thin",
+#          regular = "PublicSans-Thin.ttf")
+# 
+# font_add(family="PublicSans-Medium",
+#          regular="PublicSans-Medium.ttf")
+
+# showtext_auto()
+# showtext::showtext_opts(dpi=320)
+
+
+# css - Colors
+
+# $black: #000000;
+# $brown: #654321;
+# $tan: #d2b48c;
+# $gold: #ffd700;
+# $pink: #ffc0cb;
+# $red: #dc143c;
+# $green: #00aa00;
+# $blue: #4682b4;
+# $purple: #7e6583;
+  
+
+
+
+
+
+
+
+
+
